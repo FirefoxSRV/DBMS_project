8e181edcbfb836e201b6d414edfe49ba1e22217c
FirefoxSRV <shreyasvisweshwaran@gmail.com>
2022-12-18 21:04:18 +0530

    Project Initiated with basic UI

--- lib/tools/NameCard.dart
+++ lib/tools/NameCard.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../Screens/bioScreen.dart';
 import '../Screens/personScreen.dart';
 class NameCard extends StatelessWidget {
   final String name;
